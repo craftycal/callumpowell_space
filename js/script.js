484ab9db2f9a11dc318bf9befbaac8ae27f4be05
Callum Powell <callum@exceldp.co.nz>
2018-02-14 12:47:34 +1300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+$( document ).ready(function() {
+
+  $( "#navButton" ).click(function() {
+    $( ".navigation" ).slideToggle( "slow", function() {
+      // Animation complete.
+
+      // $( "#icon" ).click(function() {
+      //
+      // });
+
+    });
+  });
+
+
+
+
+
+
+});
